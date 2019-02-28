657444361ff718b503c5d55038424f4f30ae0e85
marnold22 <minimearnold@gmail.com>
2019-02-28 10:53:49 -0800

    Removed All Files to wipe and clean build with proper updates

--- users/includes/template/prep.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db = DB::getInstance();
-$settings = $db->query("SELECT * FROM settings")->first();
-
-require_once $abs_us_root . $us_url_root . 'usersc/templates/' . $settings->template . '/header.php'; //custom template header
-require_once $abs_us_root . $us_url_root . 'usersc/templates/' . $settings->template . '/navigation.php'; //custom template nav
-require_once $abs_us_root . $us_url_root . 'usersc/templates/' . $settings->template . '/container_open.php'; //custom template container
