8c49aa6cc58b56221ab18148b67fc0385a2d9d03
marnold22 <minimearnold@gmail.com>
2019-02-28 14:00:13 -0800

    Changed back to users navber not custom navbar

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once 'users/init.php';
 require_once $abs_us_root.$us_url_root.'users/includes/header.php';
-require_once $abs_us_root.$us_url_root.'usersc/includes/navigation.php';
+require_once $abs_us_root.$us_url_root.'users/includes/navigation.php';
 if (!securePage($_SERVER['PHP_SELF'])){die();}
 if(isset($user) && $user->isLoggedIn()){
 }
