5b0bd18dbf5b1a18effc6b78689174a997c572f0
marnold22 <minimearnold@gmail.com>
2019-03-05 13:19:08 -0800

    Added Icons and fixed on-click issue

--- usersc/includes/navigation.php
+++ usersc/includes/navigation.php
@@ -49,10 +49,7 @@ if($settings->navigation_type==0) {
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
             <li class="">
-              <a href="#" style="pointer-events: none; cursor: default;" ><?php echo echousername($user->data()->id);?></a>
-            </li>
-            <li>
-              <a href="<?=$us_url_root?>users/logout.php">Logout</a>
+              <a href="#" style="pointer-events: none; cursor: default;" ><i class="fa fa-fw fa-user"></i><?php echo echousername($user->data()->id);?></a>
             </li>
           </ul>
 
@@ -60,49 +57,37 @@ if($settings->navigation_type==0) {
           <ul class="nav navbar-nav navbar-right">
             <!-- Dropdown menu for user logged in -->
             <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Options </a>
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-fw fa-cog"></i> Options</a>
               <ul class="dropdown-menu">
 
                 <?php if (checkMenu(2,$user->data()->id)){  //Links for permission level 2 (Superuser) ?>
-                <li><a href="<?=$us_url_root?>users/admin.php">Admin Dashboard</a></li>
-                <li><a href="<?=$us_url_root?>users/admin.php?view=users">User Management</a></li>
-                <li><a href="<?=$us_url_root?>users/admin.php?view=permissions">User Permissions</a></li>
-                <li><a href="<?=$us_url_root?>users/admin.php?view=pages">System Pages</a></li>
-                <li><a href="<?=$us_url_root?>users/admin.php?view=messages">Messages Admin</a></li>
-                <li><a href="<?=$us_url_root?>users/admin.php?view=logs">System Logs</a></li>
+                <li><a href="<?=$us_url_root?>users/admin.php"><i class="fa fa-fw fa-cogs"></i> Admin Dashboard</a></li>
+                <li><a href="<?=$us_url_root?>users/admin.php?view=users"><i class="fa fa-user"></i> User Management</a></li>
+                <li><a href="<?=$us_url_root?>users/admin.php?view=permissions"><i class="fa fa-lock"></i> User Permissions</a></li>
+                <li><a href="<?=$us_url_root?>users/admin.php?view=pages"><i class="fa fa-wrench"></i> System Pages</a></li>
+                <li><a href="<?=$us_url_root?>users/admin.php?view=messages"><i class="fa fa-envelope"></i> Messages Admin</a></li>
+                <li><a href="<?=$us_url_root?>users/admin.php?view=logs"><i class="fa fa-search"></i> System Logs</a></li>
                 <li role="separator" class="divider"></li>
                 <?php } // if user is logged in ?>
 
                 <?php if (checkMenu(3,$user->data()->id)){  //Links for permission level 3 (Managers) ?>
-                <li><a href="<?=$us_url_root?>usersc/custom_list.php">Manager User List</a></li>
+                <li><a href="<?=$us_url_root?>usersc/custom_list.php"><i class="fa fa-user"></i> Manager User List</a></li>
                 <li role="separator" class="divider"></li>
                 <?php } // if user is logged in ?>
 
-                <li><a href="<?=$us_url_root?>users/logout.php">Logout</a></li>
+                <li><a href="<?=$us_url_root?>users/logout.php"><i class="fa fa-power-off"></i> Logout</a></li>
               </ul>
             </li>
           </ul>
-        </div>
 
             <?php }else{// if user is logged in ?>
 
             <!-- Dropdown menu for users not logged in -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <!-- Menu items for users not logged in -->
-          <ul class="nav navbar-nav">
-            <li class="">
-              <a href="<?=$us_url_root?>users/login.php">Login</a>
-            </li>
-            <li>
-              <a href="<?=$us_url_root?>users/join.php">Register</a>
-            </li>
-          </ul>
-          
           <ul class="nav navbar-nav navbar-right">
             <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Help </a>
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-question"></i> Help</a>
               <ul class="dropdown-menu">
-                <li><a href="<?=$us_url_root?>users/logout.php">Forgot Password</a></li>
+                <li><a href="<?=$us_url_root?>users/logout.php"><i class="fa fa-info-circle"></i> Forgot Password</a></li>
                 <?php if ($email_act){ //Only display following menu item if activation is enabled ?>
                   <li><a href="<?=$us_url_root?>users/login.php">Resend Activation Email</a></li>
                 <?php }?>
@@ -110,10 +95,22 @@ if($settings->navigation_type==0) {
             </li>
           </ul>
 
+          <!-- Menu items for users not logged in -->
+          <ul class="nav navbar-nav">
+            <li class="">
+              <a href="<?=$us_url_root?>users/login.php"><i class="fa fa-sign-in"></i> Login</a>
+            </li>
+            <li>
+              <a href="<?=$us_url_root?>users/join.php"><i class="fa fa-user-plus"></i> Register</a>
+            </li>
+          </ul>
+
       <?php } // if user is not logged in ?>
       </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
   </nav>
+
+
 
 
 <?php } //close main function ?>
