fdeb55daee3a843fcd7e316567402186fea4076e
marnold22 <minimearnold@gmail.com>
2019-03-18 13:35:33 -0700

    Added each course as a separate module

--- index.php
+++ index.php
@@ -22,124 +22,30 @@ if(isset($user) && $user->isLoggedIn()){
         </div>
     </div>
 
-    <?php echo $abs_us_root.$us_url_root.'usersc/Modules/WLS_Module.php';?>
-    <br></br>
-    <?php echo $abs_us_root.$us_url_root.'usersc/Modules/Tier2_Module.php';?>
-    <br></br>
-    <?php echo $abs_us_root.$us_url_root.'usersc/Modules/Tier3_Module.php';?>
-    <br></br>
-    <?php echo $abs_us_root.$us_url_root.'usersc/Modules/BL_Module.php';?>
+    <!--
+
+      <?php //echo $abs_us_root.$us_url_root.'usersc/Modules/WLS_Module.php';?>
+      <br></br>
+      <?php //echo $abs_us_root.$us_url_root.'usersc/Modules/Tier2_Module.php';?>
+      <br></br>
+      <?php //echo $abs_us_root.$us_url_root.'usersc/Modules/Tier3_Module.php';?>
+      <br></br>
+      <?php //echo $abs_us_root.$us_url_root.'usersc/Modules/BL_Module.php';?>
+
+    -->
 
     <!-- Include the When Lightning Strikes Module -->
-    <?php include $abs_us_root.$us_url_root.'usersc/Modules/WLS_Module.php'; ?>
+    <?php include $abs_us_root.$us_url_root.'usersc/modules/WLS_Module.php'; ?>
 
     <!-- Include the Tier 2 Module -->
-    <?php include $abs_us_root.$us_url_root.'usersc/Modules/Tier2_Module.php'; ?>
+    <?php include $abs_us_root.$us_url_root.'usersc/modules/Tier2_Module.php'; ?>
 
     <!-- Include the Tier 3 Module -->
-    <?php include $abs_us_root.$us_url_root.'usersc/Modules/Tier3_Module.php'; ?>
+    <?php include $abs_us_root.$us_url_root.'usersc/modules/Tier3_Module.php'; ?>
 
     <!-- Include the Beyond Lockdown Module -->
-    <?php include $abs_us_root.$us_url_root.'usersc/Modules/BL_Module.php'; ?>
+    <?php include $abs_us_root.$us_url_root.'usersc/modules/BL_Module.php'; ?>
 
-    <div class="container-fluid indexbar3">
-        <div class="row indexrow">
-            <div class="previewcol col-md-4">
-                <?php if ($user->data()->complete_wls == 0){ ?>
-                <img class="img-responsive" src="/usersc/images/wls.jpg" alt="Generic placeholder image">
-                <?php } ?>
-                <?php if ($user->data()->complete_wls == 1){ ?>
-                <img class="img-responsive" src="/usersc/images/wls_watched.png" alt="Generic placeholder image">
-                <?php } ?>
-            </div>
-            <div class="previewcol col-md-8">
-                <h1 class="previewtitle">When Lightning Strikes</h1>
-                <p class="previewdescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <a href="/usersc/wls_page.php" target="" class="button is-white mybutton">
-                    <?php if ($user->data()->complete_wls == 0){ ?>
-                    TAKE ME TO VIDEO
-                    <?php } ?>
-                    <?php if ($user->data()->complete_wls == 1){ ?>
-                    REWATCH VIDEO
-                    <?php } ?>
-                </a>
-            </div>
-        </div>
-    </div>
-    <div class="container-fluid indexbar3">
-        <div class="row indexrow">
-            <div class="previewcol col-md-4">
-                <?php if ($user->data()->complete_tier2 == 0){ ?>
-                <img class="img-responsive" src="/usersc/images/tier2.jpg" alt="Generic placeholder image">
-                <?php } ?>
-                <?php if ($user->data()->complete_tier2 == 1){ ?>
-                <img class="img-responsive" src="/usersc/images/tier2_watched.png" alt="Generic placeholder image">
-                <?php } ?>
-            </div>
-            <div class="previewcol col-md-8">
-                <h1 class="previewtitle">Workplace Violence Prevention and Response for Employees</h1>
-                <p class="previewdescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <a href="/usersc/tier2page.php" target="" class="button is-white mybutton">
-                    <?php if ($user->data()->complete_tier2 == 0){ ?>
-                    TAKE ME TO COURSE
-                    <?php } ?>
-                    <?php if ($user->data()->complete_tier2 == 1){ ?>
-                    REVIEW COURSE
-                    <?php } ?>
-                </a>
-            </div>
-        </div>
-    </div>
-    <?php if (checkMenu(3,$user->data()->id)){ //Links for permission (level 3 Manager) ?>
-    <div class="container-fluid indexbar3">
-        <div class="row indexrow">
-            <div class="previewcol col-md-4">
-                <?php if ($user->data()->complete_tier3 == 0){ ?>
-                <img class="img-responsive" src="/usersc/images/tier3.jpg" alt="Generic placeholder image">
-                <?php } ?>
-                <?php if ($user->data()->complete_tier3 == 1){ ?>
-                <img class="img-responsive" src="/usersc/images/tier3_watched.png" alt="Generic placeholder image">
-                <?php } ?>
-            </div>
-            <div class="previewcol col-md-8">
-                <h1 class="previewtitle">Enhanced Guidance for Managers and Leaders</h1>
-                <p class="previewdescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <a href="/usersc/tier3page.php" target="" class="button is-white mybutton">
-                    <?php if ($user->data()->complete_tier3 == 0){ ?>
-                    TAKE ME TO COURSE
-                    <?php } ?>
-                    <?php if ($user->data()->complete_tier3 == 1){ ?>
-                    REVIEW COURSE
-                    <?php } ?>
-                </a>
-            </div>
-        </div>
-    </div>
-    <?php } //is user an admin ?>
-    <div class="container-fluid indexbar3">
-        <div class="row indexrow">
-            <div class="previewcol col-md-4">
-                <?php if ($user->data()->complete_bl == 0){ ?>
-                <img class="img-responsive" src="/usersc/images/bl.jpg" alt="Generic placeholder image">
-                <?php } ?>
-                <?php if ($user->data()->complete_bl == 1){ ?>
-                <img class="img-responsive" src="/usersc/images/bl_watched.png" alt="Generic placeholder image">
-                <?php } ?>
-            </div>
-            <div class="previewcol col-md-8">
-                <h1 class="previewtitle">Beyond Lockdown</h1>
-                <p class="previewdescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <a href="/usersc/BL_page.php" target="" class="button is-white mybutton">
-                    <?php if ($user->data()->complete_bl == 0){ ?>
-                    TAKE ME TO COURSE
-                    <?php } ?>
-                    <?php if ($user->data()->complete_bl == 1){ ?>
-                    REVIEW COURSE
-                    <?php } ?>
-                </a>
-            </div>
-        </div>
-    </div>
 
 </div> <!-- /.wrapper -->
 
