5210c90f4a24ad1a44b1719e147f107e833e8824
marnold22 <minimearnold@gmail.com>
2019-03-25 12:02:29 -0700

    Export of CSV finally working
    No longer exports code from page into csv - only users list

--- usersc/export.php
+++ usersc/export.php
@@ -1,54 +1,63 @@
-<?php require_once '../users/init.php'; ?>
-<?php require_once $abs_us_root.$us_url_root.'users/includes/header.php'; ?>
-<?php require_once $abs_us_root.$us_url_root.'usersc/includes/navigation.php'; ?>
-<?php if (!securePage($_SERVER['PHP_SELF'])){die();} ?>
-
 <?php
 
-  if(isset($_POST["Export"])){
-
-    $filename = "usersCompleted_" . date('Y-m-d') . ".csv";
-
-    header('Content-Type: text/csv; charset=utf-8');
-    header('Content-Disposition: attachment; filename=' . $filename );
-
-    $output = fopen("php://output", "w");
-    fputcsv($output, array('Name', 'Email', 'Last-Sign-In', 'WPV', 'EGML', 'WLS', 'BL'));
-
-    $query = $db->query("SELECT * FROM users WHERE id IN (SELECT user_id FROM user_permission_matches WHERE permission_id = 1)");
-    $userData = $query->results();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
-    // Loop through query and to convert 0's and 1's into complete / incomplete statements for CSV
-    foreach ($userData as $person) {
+require_once '../users/init.php';
 
-      if($person->complete_tier2==0){
-        $tier2 = "incomplete";
-      }else {
-        $tier2 = "complete";
-      }
+if (!securePage($_SERVER['PHP_SELF'])){die();}
 
-      if($person->complete_tier3==0){
-        $tier3 = "incomplete";
-      }else {
-        $tier3 = "complete";
-      }
+if(isset($_POST["Export"])){
 
-      if($person->complete_wls==0){
-        $wls = "incomplete";
-      }else {
-        $wls = "complete";
-      }
+  $filename = "UserData" . date('Y-m-d') . ".csv";
+  $output = fopen("php://output", "w");
 
-      if($person->complete_bl==0){
-        $bl = "incomplete";
-      }else {
-        $bl = "complete";
-      }
+  //Set Headers of Columns
+  fputcsv($output, array('Name', 'Email', 'Last-Sign-In', 'WPV', 'EGML', 'WLS', 'BL'));
 
-      fputcsv($output, array($person->fname . " " . $person->lname, $person->email, $person->last_login, $tier2, $tier3, $wls, $bl));
+  //Run Query
+  $db = DB::getInstance();
+  $query = $db->query("SELECT * FROM users WHERE id IN (SELECT user_id FROM user_permission_matches WHERE permission_id = 1)");
+  $userData = $query->results();
+
+  // Loop through query and to convert 0's and 1's into complete / incomplete statements for CSV
+  foreach ($userData as $person) {
+
+    if($person->complete_tier2==0){
+      $tier2 = "Incomplete";
+    }else {
+      $tier2 = "Complete";
     }
-      fclose($output);
+
+    if($person->complete_tier3==0){
+      $tier3 = "Incomplete";
+    }else {
+      $tier3 = "Complete";
+    }
+
+    if($person->complete_wls==0){
+      $wls = "Incomplete";
+    }else {
+      $wls = "Complete";
+    }
+
+    if($person->complete_bl==0){
+      $bl = "Incomplete";
+    }else {
+      $bl = "Complete";
+    }
+
+    fputcsv($output, array($person->fname . " " . $person->lname, $person->email, $person->last_login, $tier2, $tier3, $wls, $bl));
   }
+
+  fclose($output);
+
+  //Set Download Data
+  header("Content-Description: File Transfer");
+  header("Content-Disposition: attachment; filename=".$filename);
+  header("Content-Type: application/csv; ");
+}
 
 ?>
