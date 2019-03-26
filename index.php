20ccd1cf83f6988d6f8679d70a2dcac7b709029b
marnold22 <minimearnold@gmail.com>
2019-03-26 12:52:01 -0700

    Cleaned up index page and removed old div's

--- index.php
+++ index.php
@@ -9,20 +9,18 @@ if(isset($user) && $user->isLoggedIn()){
 ?>
 
 <link rel="stylesheet" type="text/css" href="usersc/css/module.css">
-<div id="page-wrapper">
-    <div class="container-fluid">
-        <div class="text-center">
-            <p class="indexwelcometo">WELCOME TO</p>
-            <img src="/usersc/images/universitylogo.png" alt="..." class="">
-        </div>
-    </div>
-    <div class="container-fluid descriptionBanner">
-        <div class="text-left">
-            <p class="indexdescription">CPPS is the leading developer and provider of scalable training and consulting solutions in the U.S. for Workplace Violence Prevention, Active Shooter Response, and International Travel Safety. CPPS has worked together with thousands of organizations–large and small–to include over 50% of Fortune 100 corporations, over 1600 colleges and universities; 2000 hospitals and many of the largest non-profit/charitable organizations in the U.S.</p>
-        </div>
-    </div>
-</div> <!-- /.wrapper -->
 
+<div class="container-fluid">
+    <div class="text-center">
+        <p class="indexwelcometo">WELCOME TO</p>
+        <img src="/usersc/images/universitylogo.png" alt="..." class="">
+    </div>
+</div>
+<div class="container-fluid descriptionBanner">
+    <div class="text-left">
+        <p class="indexdescription">CPPS is the leading developer and provider of scalable training and consulting solutions in the U.S. for Workplace Violence Prevention, Active Shooter Response, and International Travel Safety. CPPS has worked together with thousands of organizations–large and small–to include over 50% of Fortune 100 corporations, over 1600 colleges and universities; 2000 hospitals and many of the largest non-profit/charitable organizations in the U.S.</p>
+    </div>
+</div>
 <div class="centered">
     <section class="cards">
 
