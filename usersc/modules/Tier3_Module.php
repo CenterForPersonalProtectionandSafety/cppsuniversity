03eeb8f3695e0e1a8cc13e1e920c4393c45bfd97
marnold22 <minimearnold@gmail.com>
2019-04-01 08:43:42 -0700

    Testing Tier 3 module fix

--- usersc/modules/Tier3_Module.php
+++ usersc/modules/Tier3_Module.php
@@ -4,21 +4,19 @@ Tier 3 Module
 Only for managers
 */
 ?>
-<?php //if (checkMenu(3,$user->data()->id)){ //Links for permission (level 3 Manager) ?>
-  <article class="card">
-      <a href="/usersc/viewtier3.php">
-          <picture class="thumbnail">
-            <?php if ($user->data()->complete_tier3 == 0){ ?>
-            <img class-"module-img" src="/usersc/images/tier3.jpg" alt="Tier 3">
-            <?php } ?>
-            <?php if ($user->data()->complete_tier3 == 1){ ?>
-            <img class-"module-img" src="/usersc/images/tier3_watched.png" alt="Tier 3 Watched">
-            <?php } ?>
-          </picture>
-          <div class="card-content">
-              <h2>Enhanced Guidance for Managers and Leaders</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          </div><!-- .card-content -->
-      </a>
-  </article><!-- .card -->
-<?php //} //is user an admin ?>
+<article class="card">
+    <a href="/usersc/viewtier3.php">
+        <picture class="thumbnail">
+          <?php if ($user->data()->complete_tier3 == 0){ ?>
+          <img class-"module-img" src="/usersc/images/tier3.jpg" alt="Tier 3">
+          <?php } ?>
+          <?php if ($user->data()->complete_tier3 == 1){ ?>
+          <img class-"module-img" src="/usersc/images/tier3_watched.png" alt="Tier 3 Watched">
+          <?php } ?>
+        </picture>
+        <div class="card-content">
+            <h2>Enhanced Guidance for Managers and Leaders</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div><!-- .card-content -->
+    </a>
+</article><!-- .card -->
