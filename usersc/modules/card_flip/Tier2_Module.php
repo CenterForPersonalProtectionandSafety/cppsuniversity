afecfb782c163c2975af2e8c4f1b74260a4dff85
marnold22 <minimearnold@gmail.com>
2019-04-02 09:44:01 -0700

    Forgot one module

--- usersc/modules/card_flip/Tier2_Module.php
+++ usersc/modules/card_flip/Tier2_Module.php
@@ -6,16 +6,14 @@ Tier 2 Module
 <div class="card">
     <input type="checkbox" id="card2" class="more">
     <div class="content">
-        <div class="front" style="background-image: url('https://images.unsplash.com/photo-1515263487990-61b07816b324?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c02fb96f9cfc16d3649835b75d1b2033&auto=format&fit=crop&w=1350&q=80')">
+      <?php if ($user->data()->complete_tier2 == 0){ ?>
+      <div class="front" style="background-image: url('/usersc/images/tier2.jpg')">
+      <?php } ?>
+      <?php if ($user->data()->complete_tier2 == 1){ ?>
+      <div class="front" style="background-image: url('/usersc/images/tier2_watched.png')">
+      <?php } ?>
             <div class="inner">
-                <h2>Modern flat</h2>
-                <div class="rating">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star-half-alt"></i>
-                    <i class="far fa-star"></i>
-                </div>
+                <h2>Workplace Violence Prevention and Response for Employees</h2>
                 <label for="card2" class="button" aria-hidden="true">
                     Details
                 </label>
@@ -23,51 +21,16 @@ Tier 2 Module
         </div>
         <div class="back">
             <div class="inner">
-                <div class="info">
-                    <span>4</span>
-                    <div class="icon">
-                        <i class="fas fa-users"></i>
-                        <span>people</span>
-                    </div>
-                </div>
-                <div class="info">
-                    <span>3</span>
-                    <div class="icon">
-                        <i class="fas fa-door-open"></i>
-                        <span>rooms</span>
-                    </div>
-                </div>
-                <div class="info">
-                    <span>2</span>
-                    <div class="icon">
-                        <i class="fas fa-bed"></i>
-                        <span>beds</span>
-                    </div>
-                </div>
-                <div class="info">
-                    <span>1</span>
-                    <div class="icon">
-                        <i class="fas fa-bath"></i>
-                        <span>bath</span>
-                    </div>
-                </div>
                 <div class="description">
-                    <p>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem unde ea voluptas
-                        fuga
-                        odio vel veniam eveniet, explicabo autem earum?</p>
-                    <ul>
-                        <li>Lorem ipsum dolor sit amet.</li>
-                        <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</li>
-                        <li>Lorem ipsum dolor sit amet consectetur.</li>
-                        <li>Lorem ipsum dolor sit amet.</li>
-                    </ul>
+                  <h4>Workplace Violence Prevention and Response for Employees</h4>
+                  <p>Research shows that over 2 million people are affected each year by violent victimizations in the workplace at a cost of over 100 billion dollars to U.S. corporations annually.  Violence, however, is almost always evolutionary with warning signs along the way.</p>
                 </div>
-                <div class="location">Warsaw, Poland</div>
-                <div class="price">42€ / day</div>
                 <label for="card2" class="button return" aria-hidden="true">
-                    <i class="fas fa-arrow-left"></i>
+                    <i class="fa fa-arrow-left"></i>
                 </label>
+                <a href="/usersc/viewtier2.php" class="button return button-play" aria-hidden="true">
+                  <i class="fa fa-play"></i>
+                </a>
             </div>
         </div>
     </div>
