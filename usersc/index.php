982d944756830ad971c9ba87dda7f2de17167d6a
Michael Arnold <michaelarnold@Michaels-MacBook-Pro.local>
2019-11-19 10:44:07 -0800

    updated paths to about us images

--- usersc/index.php
+++ usersc/index.php
@@ -31,7 +31,7 @@ if($user->data()->first_login_pass_reset == 0) {
 	      <div class="aboutcard">
 	          <input type="checkbox" class="more" aria-hidden="true">
 	          <div class="content">
-	              <div class="front" style="background-image: url('/usersc/images/company.png')">
+	              <div class="front" style="background-image: url('/usersc/images/aboutus/company.png')">
 	                  <div class="inner">
 	                      <h4>50% of Fortune 100 corporations</h4>
 	                  </div>
@@ -41,7 +41,7 @@ if($user->data()->first_login_pass_reset == 0) {
 	      <div class="aboutcard">
 	          <input type="checkbox" class="more" aria-hidden="true">
 	          <div class="content">
-	              <div class="front" style="background-image: url('/usersc/images/university.png')">
+	              <div class="front" style="background-image: url('/usersc/images/aboutus/university.png')">
 	                  <div class="inner">
 	                      <h4>1600 colleges and universities</h4>
 	                  </div>
@@ -51,7 +51,7 @@ if($user->data()->first_login_pass_reset == 0) {
 	      <div class="aboutcard">
 	          <input type="checkbox" class="more" aria-hidden="true">
 	          <div class="content">
-	              <div class="front" style="background-image: url('/usersc/images/hospital.png')">
+	              <div class="front" style="background-image: url('/usersc/images/aboutus/hospital.png')">
 	                  <div class="inner">
 	                      <h4>2000 hospitals & many non-profit organizations</h4>
 	                  </div>
